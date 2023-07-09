4158906b0e024f510aff650509866bbbfc6d2cae
MikuroXina <ryosukadnak@gmail.com>
2023-07-10 01:49:40 +0900

    Add some conversion

--- src/option.ts
+++ src/option.ts
@@ -2,7 +2,8 @@
 // ===  ((() + T) => I) => I
 // ===  ((() => I) × (T => I)) => I
 // ===  (() => I) => (T => I) => I
-import { type Bool, FALSE, TRUE } from "./bool.js";
+import { type Bool, FALSE, TRUE, ifThenElse } from "./bool.js";
+import { type List, nil } from "./list.js";
 
 // ===  I => (T => I) => I
 export type Option<T> = <I>(onNone: I) => (onSome: (value: T) => I) => I;
@@ -18,5 +19,16 @@ export const some =
     (onSome) =>
         onSome(value);
 
+export const fromPredicate =
+    <T>(pred: (t: T) => Bool) =>
+    (t: T): Option<T> =>
+        ifThenElse(pred(t))(some(t))(none());
+
 export const isNone = <T>(opt: Option<T>): Bool => opt(TRUE)(() => FALSE);
 export const isSome = <T>(opt: Option<T>): Bool => opt(FALSE)(() => TRUE);
+
+export const intoList =
+    <T>(opt: Option<T>): List<T> =>
+    (onNil) =>
+    (onCons) =>
+        opt(onNil)((value) => onCons(value)(nil()));
