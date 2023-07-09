684f37e595173aebf4a0c86766209d457c39a63b
MikuroXina <ryosukadnak@gmail.com>
2023-07-10 02:20:48 +0900

    Fix Option type

--- src/option.ts
+++ src/option.ts
@@ -1,19 +1,18 @@
-// () + T
-// ===  ((() + T) => I) => I
-// ===  ((() => I) × (T => I)) => I
-// ===  (() => I) => (T => I) => I
 import { type Bool, FALSE, TRUE, and as boolAnd, ifThenElse } from "./bool.js";
 import { type List, nil } from "./list.js";
 import { type Pair, first, newPair, second } from "./pair.js";
 
-// ===  I => (T => I) => I
-export type Option<T> = <I>(onNone: I) => (onSome: (value: T) => I) => I;
+// () + T
+// ===  ((() + T) => I) => I
+// ===  ((() => I) × (T => I)) => I
+// ===  (() => I) => (T => I) => I
+export type Option<T> = <I>(onNone: () => I) => (onSome: (value: T) => I) => I;
 
 export const none =
     <T>(): Option<T> =>
     (onNone) =>
     () =>
-        onNone;
+        onNone();
 export const some =
     <T>(value: T): Option<T> =>
     () =>
@@ -25,35 +24,35 @@ export const fromPredicate =
     (t: T): Option<T> =>
         ifThenElse(pred(t))(some(t))(none());
 
-export const isNone = <T>(opt: Option<T>): Bool => opt(TRUE)(() => FALSE);
-export const isSome = <T>(opt: Option<T>): Bool => opt(FALSE)(() => TRUE);
+export const isNone = <T>(opt: Option<T>): Bool => opt(() => TRUE)(() => FALSE);
+export const isSome = <T>(opt: Option<T>): Bool => opt(() => FALSE)(() => TRUE);
 
 export const intoList =
     <T>(opt: Option<T>): List<T> =>
     (onNil) =>
     (onCons) =>
-        opt(onNil)((value) => onCons(value)(nil()));
+        opt(() => onNil)((value) => onCons(value)(nil()));
 
 export const flatten = <T>(optOpt: Option<Option<T>>): Option<T> =>
-    optOpt(none<T>())((value) => value);
+    optOpt(none<T>)((value) => value);
 
 export const and =
     <B>(optB: Option<B>) =>
     <A>(optA: Option<A>): Option<B> =>
-        optA(none<B>())(() => optB);
+        optA(none<B>)(() => optB);
 export const andThen =
     <A, B>(optB: (a: A) => Option<B>) =>
     (optA: Option<A>): Option<B> =>
-        optA(none<B>())(optB);
+        optA(none<B>)(optB);
 
 export const or =
     <T>(optB: Option<T>) =>
     (optA: Option<T>): Option<T> =>
-        optA(optA)(() => optB);
+        optA(() => optA)(() => optB);
 export const orElse =
     <T>(optB: () => Option<T>) =>
     (optA: Option<T>): Option<T> =>
-        optA(optA)(optB);
+        optA(() => optA)(optB);
 
 export const xor =
     <T>(optB: Option<T>) =>
@@ -65,18 +64,20 @@ export const xor =
 export const filter =
     <T>(pred: (t: T) => Bool) =>
     (opt: Option<T>): Option<T> =>
-        ifThenElse(opt(FALSE)(pred))(opt)(none());
+        ifThenElse(opt(() => FALSE)(pred))(opt)(none());
 
 export const zip =
     <A>(optA: Option<A>) =>
     <B>(optB: Option<B>): Option<Pair<A, B>> =>
-        optA(none<Pair<A, B>>())((a) => optB(none<Pair<A, B>>())((b) => some(newPair(a)(b))));
+        optA(none<Pair<A, B>>)((a) => optB(none<Pair<A, B>>)((b) => some(newPair(a)(b))));
 
 export const unzip = <A, B>(optA: Option<Pair<A, B>>): Pair<Option<A>, Option<B>> =>
-    optA(newPair(none<A>())(none<B>()))((pair) => newPair(some(first(pair)))(some(second(pair))));
+    optA(() => newPair(none<A>())(none<B>()))((pair) =>
+        newPair(some(first(pair)))(some(second(pair))),
+    );
 
 export const zipWith =
     <A, B, X>(fn: (a: A) => (b: B) => X) =>
     (optA: Option<A>) =>
     (optB: Option<B>): Option<X> =>
-        optA(none<X>())((a) => optB(none<X>())((b) => some(fn(a)(b))));
+        optA(none<X>)((a) => optB(none<X>)((b) => some(fn(a)(b))));
