0d41743279b54b51d34b803a6f8fbabdf72a7b2e
MikuroXina <ryosukadnak@gmail.com>
2023-07-10 02:35:32 +0900

    Add conversions

--- src/result.ts
+++ src/result.ts
@@ -19,3 +19,14 @@ export const err =
 
 export const isOk = <E, T>(res: Result<E, T>): Bool => res(() => FALSE)(() => TRUE);
 export const isErr = <E, T>(res: Result<E, T>): Bool => res(() => TRUE)(() => FALSE);
+
+export const either =
+    <I, E>(onErr: (error: E) => I) =>
+    <T>(onOk: (value: T) => I) =>
+    (res: Result<E, T>): I =>
+        res(onErr)(onOk);
+
+export const flatten = <E, T>(resRes: Result<E, Result<E, T>>): Result<E, T> =>
+    resRes(err<E, T>)((value) => value);
+
+export const mergeOkErr = <T>(res: Result<T, T>): T => res((x) => x)((x) => x);
