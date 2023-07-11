0ab1de00f37f360e00439a4ec31cc98e819659b0
MikuroXina <ryosukadnak@gmail.com>
2023-07-12 02:29:39 +0900

    Improve Bool

--- src/bool.ts
+++ src/bool.ts
@@ -1,30 +1,39 @@
-export type Bool = <X>(x: X) => <Y>(y: Y) => X | Y;
+import { type Empty, empty } from "./empty.js";
+import { type Unit, unit } from "./unit.js";
+
+// TRUE + FALSE
+// ((TRUE + FALSE) => I) => I
+// ((TRUE => I) × (FALSE => I)) => I
+// (TRUE => I) => (FALSE => I) => I
+export type Bool = <I>(onTrue: () => I) => (onFalse: () => I) => I;
 
 export const TRUE: Bool =
-    <X>(x: X) =>
-    <Y>(_y: Y): X =>
-        x;
+    <I>(x: () => I) =>
+    (): I =>
+        x();
 export const FALSE: Bool =
-    <X>(_x: X) =>
-    <Y>(y: Y): Y =>
-        y;
+    <I>() =>
+    (y: () => I): I =>
+        y();
 
 export const ifThenElse =
     (condition: Bool) =>
     <T>(ifTrue: T) =>
-    <F>(ifFalse: F) =>
-        condition(ifTrue)(ifFalse);
+    (ifFalse: T) =>
+        condition<T>(() => ifTrue)(() => ifFalse);
 
 export const and =
     (p: Bool) =>
     (q: Bool): Bool =>
-        p(q)(p);
+        p(() => q)(() => p);
 export const or =
     (p: Bool) =>
     (q: Bool): Bool =>
-        p(p)(q);
-export const not = (p: Bool): Bool => p(FALSE)(TRUE);
+        p(() => p)(() => q);
+export const not = (p: Bool): Bool => p(() => FALSE)(() => TRUE);
 export const xor =
     (p: Bool) =>
     (q: Bool): Bool =>
-        p(not(q))(q);
+        p(() => not(q))(() => q);
+
+export const asType = (p: Bool): Unit | Empty => ifThenElse(p)(unit)(empty);
