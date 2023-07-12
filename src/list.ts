1323417223093fe09c0bb7b1baf0c9f1271cdc77
MikuroXina <ryosukadnak@gmail.com>
2023-07-12 15:03:25 +0900

    Fix head

--- src/list.ts
+++ src/list.ts
@@ -1,5 +1,5 @@
 import { type Bool, FALSE, TRUE } from "./bool.js";
-import type { Option } from "./option.js";
+import { type Option, none, some } from "./option.js";
 
 export type List<T> = <I>(onNil: I) => (onCons: (head: T) => (tail: List<T>) => I) => I;
 
@@ -23,11 +23,8 @@ export const foldR =
 
 export const isEmpty: <T>(list: List<T>) => Bool = foldR(TRUE)(() => () => FALSE);
 
-export const head =
-    <T>(list: List<T>): Option<T> =>
-    (onNone) =>
-    (onSome) =>
-        list(onNone)((headItem) => () => onSome(headItem));
+export const head = <T>(list: List<T>): Option<T> =>
+    list(none<T>())((headItem) => () => some(headItem));
 
 export const tail =
     <T>(list: List<T>): List<T> =>
