7c91bdd07b583d10863d4e312b74e0919502a5cb
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-04-19 07:40:32 +0900

    More methods for List

--- src/list.ts
+++ src/list.ts
@@ -1,4 +1,5 @@
 import { type Bool, FALSE, TRUE } from "./bool.js";
+import { type Nat, isZero, pred } from "./nat.js";
 import { type Option, none, some } from "./option.js";
 
 export type List<T> = <I>(onNil: I) => (onCons: (head: T) => (tail: List<T>) => I) => I;
@@ -31,3 +32,27 @@ export const tail =
     (onNil) =>
     (onCons) =>
         list(onNil)(() => (tailItem) => tailItem(onNil)(onCons));
+
+export const map =
+    <T, U>(mapper: (item: T) => U) =>
+    (list: List<T>): List<U> =>
+    (onNil) =>
+    (onCons) =>
+        list(onNil)((head) => (tail) => onCons(mapper(head))(map(mapper)(tail)));
+
+export const singleton = <T>(item: T): List<T> => cons(item)(nil());
+
+export const at =
+    (index: Nat) =>
+    <T>(list: List<T>): Option<T> =>
+        list(none<T>())(
+            (head) => (tail) => isZero(index)(() => some(head))(() => at(pred(index))(tail)),
+        );
+
+export const withIndex =
+    (index: Nat) =>
+    <T>(toWrite: T) =>
+    (list: List<T>): List<T> =>
+        isZero(index)(() => cons(toWrite)(tail(list)))(() =>
+            list(list)((h) => (t) => cons(h)(withIndex(pred(index))(toWrite)(t))),
+        );
